37609276f2596a6d83f33923568fa371af3ebd30
github username <github email address>
2019-03-25 09:57:45 -0400

    sets up sign up submit handler

--- src/App.js
+++ src/App.js
@@ -18,6 +18,27 @@ class App extends Component {
     user: {}
   }
 
+// Sign up submit handler posts userInfo (which is the current sign up form state) to
+// our backend API. It also saves a jwt token to the local storage and pushes the
+// user to "/create-task"
+  signupSubmitHandler = (userInfo) => {
+    fetch( "#", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "accepts": "application/json"
+      },
+      body: JSON.stringify(userInfo)
+    })
+      .then(resp => resp.json())
+      .then(userData => {
+        this.setState({ user: userData.user}, () => {
+          localStorage.setItem("token", userData.jwt);
+          this.props.history.push("/create-task");
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,7 +47,7 @@ class App extends Component {
           <Route path="/create-task" render={() => <CreateTask user={this.state.user}/>} />
           <Route path="/current-task" render={() => <CurrentTask user={this.state.user}/>} />
           <Route path="/signup" render={() => <Login user={this.state.user}/>} />
-          <Route path="/login" render={() => <Signup user={this.state.user}/>} />
+          <Route path="/login" render={() => <Signup signupSubmitHandler={this.signupSubmitHandler} user={this.state.user}/>} />
           <Route exact path="/" component={Home}/>
         </Switch>
 
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
 class Signup extends React.Component {
+
+state = {
+  email: "",
+  password: ""
+}
+
+changeHandler = e => {
+  this.setState({
+    [e.target.placeholder]: e.target.value
+  })
+}
+
+signupSubmitHandler = e => {
+  e.preventDefault();
+  this.props.userSubmitHandler(this.state);
+  this.setState({
+    username: "",
+    password: ""
+  })
+}
+
+
 render() {
   return (
     <div className="signup">
       <h1>Sign up</h1>
+      <form onSubmit={this.signupSubmitHandler}>
+        <input type="text" placeholder="email"
+        value={this.state.username} onChange={this.changeHandler} />
+        <input type="password" placeholder="password"
+        value={this.state.password} onChange={this.changeHandler} />
+        <button>Sign Up</button>
+      </form>
     </div>
   )
 }
